3fd0433240da762f0e07990a240137f2ba165efd
ambli <ambliach@gmail.com>
2019-07-28 16:40:43 -0400

    Updates

---
+++ precache-manifest.344c1a0f0bfb89af331b9b6ad2823bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c7059323f5abeeafd66430653d6466",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "8665170dccb7cf716558",
+    "url": "/clickygame/static/css/main.e07a1332.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clickygame/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "8665170dccb7cf716558",
+    "url": "/clickygame/static/js/main.dc5d9faa.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
